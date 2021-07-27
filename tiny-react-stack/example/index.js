efbcee83f2d4a038baa6349666aafe07f2dc2257
muronggoudan <78434793@qq.com>
2021-07-28 00:08:20 +0800

    refactor: 目录结构调整

--- tiny-react-stack/example/index.js
+++ tiny-react-stack/example/index.js
@@ -1,6 +1,6 @@
-import TinyReact from "./TinyReact"
+import TinyReact from "../src/index";
 
-const root = document.getElementById("root")
+const root = document.getElementById("root");
 
 const virtualDOM = (
   <div className="container">
@@ -18,7 +18,7 @@ const virtualDOM = (
     2, 3
     <input type="text" value="13" />
   </div>
-)
+);
 
 const modifyDOM = (
   <div className="container">
@@ -33,7 +33,7 @@ const modifyDOM = (
     <button onClick={() => alert("你好!!!!!")}>点击我</button>
     <input type="text" value="13" />
   </div>
-)
+);
 
 // TinyReact.render(virtualDOM, root)
 
@@ -42,7 +42,7 @@ const modifyDOM = (
 // }, 2000)
 
 function Demo() {
-  return <div>Hello</div>
+  return <div>Hello</div>;
 }
 
 function Heart(props) {
@@ -51,30 +51,30 @@ function Heart(props) {
       {props.title}
       &hearts; <Demo />
     </div>
-  )
+  );
 }
 
 // TinyReact.render(<Heart title="Hello React" />, root)
 
 class Alert extends TinyReact.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      title: "Default Title"
-    }
-    this.handleClick = this.handleClick.bind(this)
+      title: "Default Title",
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
-    this.setState({ title: "Changed Title" })
+    this.setState({ title: "Changed Title" });
   }
   componentWillReceiveProps(nextProps) {
-    console.log("componentWillReceiveProps")
+    console.log("componentWillReceiveProps");
   }
   componentWillUpdate() {
-    console.log("componentWillUpdate")
+    console.log("componentWillUpdate");
   }
   componentDidUpdate() {
-    console.log("componentDidUpdate")
+    console.log("componentDidUpdate");
   }
   render() {
     return (
@@ -86,7 +86,7 @@ class Alert extends TinyReact.Component {
           <button onClick={this.handleClick}>改变Title</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -99,28 +99,28 @@ class Alert extends TinyReact.Component {
 
 class DemoRef extends TinyReact.Component {
   constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
     // console.log(this.input.value)
-    console.log(this.input)
-    console.log(this.alert)
+    console.log(this.input);
+    console.log(this.alert);
   }
   componentDidMount() {
-    console.log("componentDidMount")
+    console.log("componentDidMount");
   }
   componentWillUnmount() {
-    console.log("componentWillUnmount")
+    console.log("componentWillUnmount");
   }
   render() {
     return (
       <div>
-        <input type="text" ref={input => (this.input = input)} />
+        <input type="text" ref={(input) => (this.input = input)} />
         <button onClick={this.handleClick}>按钮</button>
-        <Alert ref={alert => (this.alert = alert)} name="张三" age={20} />
+        <Alert ref={(alert) => (this.alert = alert)} name="张三" age={20} />
       </div>
-    )
+    );
   }
 }
 
@@ -128,41 +128,41 @@ class DemoRef extends TinyReact.Component {
 
 class KeyDemo extends TinyReact.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       persons: [
         {
           id: 1,
-          name: "张三"
+          name: "张三",
         },
         {
           id: 2,
-          name: "李四"
+          name: "李四",
         },
         {
           id: 3,
-          name: "王五"
+          name: "王五",
         },
         {
           id: 4,
-          name: "赵六"
-        }
-      ]
-    }
-    this.handleClick = this.handleClick.bind(this)
+          name: "赵六",
+        },
+      ],
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
-    const newState = JSON.parse(JSON.stringify(this.state))
+    const newState = JSON.parse(JSON.stringify(this.state));
     // newState.persons.push(newState.persons.shift())
     // newState.persons.splice(1, 0, { id: 100, name: "李逵" })
-    newState.persons.pop()
-    this.setState(newState)
+    newState.persons.pop();
+    this.setState(newState);
   }
   render() {
     return (
       <div>
         <ul>
-          {this.state.persons.map(person => (
+          {this.state.persons.map((person) => (
             <li key={person.id}>
               {person.name}
               <DemoRef />
@@ -171,8 +171,8 @@ class KeyDemo extends TinyReact.Component {
         </ul>
         <button onClick={this.handleClick}>按钮</button>
       </div>
-    )
+    );
   }
 }
 
-TinyReact.render(<KeyDemo />, root)
+TinyReact.render(<KeyDemo />, root);
